192c50d10f58b6c5f3548a9913c895eafba6259e
sahalr1 <mohammed.sahal@lxisoft.com>
2021-03-14 18:15:03 +0530

    Initial version of thasheel generated by JHipster-6.10.0

---
+++ src/main/java/com/thasheel/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.thasheel.service.dto;
---
+++ src/main/java/com/thasheel/web/websocket/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Access Objects used by WebSocket services.
+ */
+package com.thasheel.web.websocket.dto;
